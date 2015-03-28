24735e16bc92665d52d4b7ecebcc70219688e832
RealJohnGalt <johngaltfirstrun@gmail.com>
2015-03-28 07:27:02 -0700

    switch trader over to config.ini

--- trader.py
+++ trader.py
@@ -1,15 +1,18 @@
 import asyncio
 import logging
-import time
 
 import exchangelayer as el
-import genconfig as gc
 import genutils as gu
 import strategies as st
 import trader as trd
+from storage import config
+
 
 FreshOrder = False
 LastOrder = {}
+Asset = config.gc['API']['Trade Pair'][:3]
+Currency = config.gc['API']['Trade Pair'][-3:]
+
 
 logger = logging.getLogger('trader')
 
@@ -44,8 +47,8 @@ def TradeWrapper():
         PriceDelta = max(Prices) - min(Prices)
         TradeAmount = GetTradeAmount(
             trd.LastOrder['order'], trd.LastOrder['tradevolume'])
-        if PriceDelta <= (gc.Trader.ReIssueSlippage / 100) * trd.LastOrder['price']:
-          if TradeAmount > gc.API.AssetTradeMin:
+        if PriceDelta <= (float(config.gc['Trader']['ReIssue Slippage']) / 100) * trd.LastOrder['price']:
+          if TradeAmount > float(config.gc['API']['Asset Trade Minimum']):
             el.Trade(trd.LastOrder['order'], CurrPrice, TradeAmount)
             logger.debug(
                 'Re %s at %s', trd.LastOrder['order'].upper(), str(CurrPrice))
@@ -56,31 +59,31 @@ def TradeWrapper():
         # Not a new order, and no existing orders. Stop loop.
         logger.debug('Order Successful')
         trd.LastOrder = {}
-    yield from asyncio.sleep(gc.Trader.ReIssueDelay)
+    yield from asyncio.sleep(int(config.gc['Trader']['ReIssue Delay']))
 
 
 def TradeFromStrategy():
   if st.Trade_dict['Order'] == 'Buy':
     trd.FreshOrder = True
     TradeAmount = GetTradeAmount('buy', st.Trade_dict['TradeVolume'])
-    if TradeAmount > gc.API.AssetTradeMin:
-      logger.debug('BUYING %s %s at %s %s', str(TradeAmount), gc.API.Asset, str(
-          el.GetMarketPrice('ask')), gc.API.Currency)
+    if TradeAmount > float(config.gc['API']['Asset Trade Minimum']):
+      logger.debug('BUYING %s %s at %s %s', str(TradeAmount), Asset, str(
+          el.GetMarketPrice('ask')), Currency)
       trd.LastOrder = {
           'order': 'buy', 'price': el.GetMarketPrice('ask'),
           'amount': TradeAmount, 'tradevolume': st.Trade_dict['TradeVolume']}
     else:
       logger.debug('Wanted to BUY %s %s at %s but needed more %s', str(
-          TradeAmount), gc.API.Asset, str(el.GetMarketPrice('bid')), gc.API.Currency)
+          TradeAmount), Asset, str(el.GetMarketPrice('bid')), Currency)
   elif st.Trade_dict['Order'] == 'Sell':
     trd.FreshOrder = True
     TradeAmount = GetTradeAmount('sell', st.Trade_dict['TradeVolume'])
-    if TradeAmount > gc.API.AssetTradeMin:
-      logger.debug('SELLING %s %s at %s %s', str(TradeAmount), gc.API.Asset, str(
-          el.GetMarketPrice('bid')), gc.API.Currency)
+    if TradeAmount > float(config.gc['API']['Asset Trade Minimum']):
+      logger.debug('SELLING %s %s at %s %s', str(TradeAmount), Asset, str(
+          el.GetMarketPrice('bid')), Currency)
       trd.LastOrder = {
           'order': 'sell', 'price': el.GetMarketPrice('bid'),
           'amount': TradeAmount, 'tradevolume': st.Trade_dict['TradeVolume']}
     else:
       logger.debug('Wanted to SELL %s %s at %s but needed more %s', str(
-          TradeAmount), gc.API.Asset, str(el.GetMarketPrice('bid')), gc.API.Asset)
+          TradeAmount), Asset, str(el.GetMarketPrice('bid')), Asset)
